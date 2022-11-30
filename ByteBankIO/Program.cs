5221a62247f79006744579e627b64a93aa133a1d
guusluiz <gustavoluizmiranda405@gmail.com>
2022-11-30 19:54:37 -0300

    segundo commit

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,43 +1,28 @@
 ﻿using ByteBankIO;
 using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
-        
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
 
-        var buffer = new byte[1024];
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDoArquivo);
 
-        numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            //var linha = leitor.ReadLine();
 
-        while(numeroDeBytesLidos != 0)
-        { 
-            fluxoDoArquivo.Read(buffer, 0, 1024); //1KB
-            EscreverBuffer(buffer);
+            //var texto = leitor.ReadToEnd();
+
+            //var numero = leitor.Read();
+
+            while(!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
         }
-
-        
-        // public override int Read(byte[] array, int offset, int count);
-
         Console.ReadLine();
-    }
-
-    static void EscreverBuffer(byte[] buffer)
-    {
-
-        var utf8 = new UTF8Encoding();
-
-        var texto = utf8.GetString(buffer);
-        Console.Write(texto);
-
-        //foreach (var meuByte in buffer)
-        //{
-        //    Console.Write(meuByte);
-        //    Console.Write(" ");
-        //}
-    }
+    }  
 }
